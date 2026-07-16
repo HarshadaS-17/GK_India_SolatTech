6196c4016695f851aab0b26278cdf6b8c5b40b57
MINGJI <deorukhkarshruti232@gmail.com>
2026-07-16 18:16:25 +0530

    Changes

--- solarTech/src/routes/AppRoutes.tsx
+++ solarTech/src/routes/AppRoutes.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 // import About from "../pages/About/About";
 // import Services from "../pages/Services/Services";
-// import Contact from "../pages/Contact/Contact";
+import Contact from "../pages/Contact/Contact";
 import NotFound from "../pages/NotFound/NotFound";
 import About from "../pages/About/About";
 
@@ -14,11 +14,12 @@ const AppRoutes = () => {
 
       <Route path="/about" element={<About />} />
 
-      {/* <Route path="/services" element={<Services />} />
-
       <Route path="/contact" element={<Contact />} />
 
-      {/* 404 */}
+      {/* <Route path="/services" element={<Services />} />
+
+
+{/* 404 */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
